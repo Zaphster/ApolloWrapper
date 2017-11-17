782cd9af9336468d03f3ca4296cc123c2cdbfc07
Cameron Barker <cameron.barker42@gmail.com>
2017-11-17 16:28:10 -0600

    Initial Commit

---
+++ ApolloWrapper/ApolloWrapper.h
@@ -0,0 +1,19 @@
+//
+//  ApolloWrapper.h
+//  ApolloWrapper
+//
+//  Created by Cameron Barker on 11/17/17.
+//  Copyright © 2017 Cameron Barker Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApolloWrapper.
+FOUNDATION_EXPORT double ApolloWrapperVersionNumber;
+
+//! Project version string for ApolloWrapper.
+FOUNDATION_EXPORT const unsigned char ApolloWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApolloWrapper/PublicHeader.h>
+
+
---
+++ ApolloWrapper/ApolloWrapper.swift
@@ -0,0 +1,13 @@
+//
+//  ApolloWrapper.swift
+//  ApolloWrapper
+//
+//  Created by Cameron Barker on 11/17/17.
+//  Copyright © 2017 Cameron Barker Training. All rights reserved.
+//
+
+import Foundation
+
+class ApolloWrapper:ApolloClient{
+    
+}
